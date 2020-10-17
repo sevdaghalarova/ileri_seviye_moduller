400ec72178fa9176038d850a8a3f92863e94a25d
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-18 01:54:19 +0400

    Add files via upload

---
+++ sys_modulu.py
@@ -0,0 +1,6 @@
+import sys
+sys.stderr.write("Bu hata mesajidir\n") #hata mesaji yazar
+sys.stderr.flush() # bu mesaji ekrana bastirir
+sys.stdout.write("Bu normal mesajdir\n") #normal mesaj yazar
+sys.argv # terminalda calismak icin kullanilir
+sys.exit() #programi sonlandirmak icin kullanilir
